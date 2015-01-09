982030458ec831ecc4b5db1a68e78921818b13a1
Richard Littauer <richard.littauer@gmail.com>
2015-01-08 16:09:50 -0800

    Added title arg, changed location

--- README.md
+++ README.md
@@ -9,18 +9,16 @@ journall
 
 ## Usage
 
-Journall will use the `entries/` folder, and save a file titled with the date. It won't make a new file if today's date is already taken. It also adds in a brief header. 
+`journall 'Title of post'`
+
+Journall will use the `Documents/journall/` folder, and save a file titled with the date. It won't make a new file if today's date is already taken. It also adds in the date as a header, and can add a secondary header with the first argument (or the `-t` or `--title` arg).
 
 The idea is that typing `journall` automatically constructs the file for you, so you don't have to build one yourself, and so you always have a markdown file you can write anything in that is persistently saved so you don't end up with hundreds of random files in a folder called 'journal'.
 
-Nativelly, this package supports [iA Writer](http://www.iawriter.com/mac/), which is the best Markdown editor I've found for OS X. That will have to be installed for this to work. 
-
-To make everything easier, add this to your .bash_profile: 
-
-`alias journall='node /path/to/dir/journall/index.js'` 
+Natively, this package supports [iA Writer](http://www.iawriter.com/mac/), which is the best Markdown editor I've found for OS X. That will have to be installed for this to work. This package does not work on Windows. 
 
 From then on, simply type `$ journall` and today's date should open.
 
 ## Contributing
 
-Feel free. I've kept it simple, but it doesn't have to be.
+Feel free. 
---
+++ bin/index.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+'use strict';
+
+var fs = require('fs')
+var walk = require('walk')
+var moment = require('moment')
+var _ = require('underscore')
+var path = require('path')
+var shell = require('shelljs')
+var argv = require('minimist')(process.argv.slice(2), {
+    alias: {
+        t: 'title',
+    }
+})
+
+var journalFolder = (path.resolve(process.env['HOME'], 'Documents/journall'))
+var files  = []
+var filename = moment().format('YYYY.MM.DD') + '.md'
+
+// Read all files
+var walker  = walk.walk(journalFolder, { followLinks: false })
+walker.on('file', function(root, stat, next) {
+    files.push(stat.name)
+    next()
+})
+
+walker.on('end', function() {
+
+  var header
+
+  if (!_.contains(files, filename)) {
+
+    // Add the date and an optional title to the header
+    header = '# ' + moment().format('YYYY MMMM Do') + '\n'
+    if (argv.t && typeof argv.t === 'string' ||
+      argv._[0] && typeof argv._[0] === 'string') {
+      header += '\n## ' + (argv.t || argv._[0]) + '\n'
+    }
+
+    // Create file with simple header
+    fs.writeFile(journalFolder + '/' + filename, header, function(err) {
+      if (err) { console.log(err) }
+      else {
+        // Open file
+        console.log("Log entered. Opening page.")
+        shell.exec('open -a "iA Writer" ' + journalFolder + '/' + filename,
+          {silent: false}).output
+      }
+    })
+  } else {
+
+    // Add the optional title at the end of the file
+    if (argv.t && typeof argv.t === 'string' ||
+      argv._[0] && typeof argv._[0] === 'string') {
+      header = '\n\n## ' + (argv.t || argv._[0]) + '\n'
+
+      fs.appendFile(journalFolder + '/' + filename, header, function(err) {
+        if (err) console.log('Writing the new header did not work', err)
+      })
+    }
+
+    // Open file
+    console.log('Date already exists! Opening page.')
+    shell.exec('open -a "iA Writer" ' + journalFolder + '/' + filename,
+      {silent: false}).output
+  }
+})
