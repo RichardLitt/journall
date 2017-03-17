52ce77cbfec37f7397826b14411215776fe09bc7
Richard Littauer <richard.littauer@gmail.com>
2017-03-16 20:39:40 -0400

    Added meow, refactored a bit
    Closes #4

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 #!/usr/bin/env node
-'use strict';
 
-var fs = require('fs')
-var walk = require('walk')
-var moment = require('moment')
-var _ = require('underscore')
-var path = require('path')
-var shell = require('shelljs')
-var argv = require('minimist')(process.argv.slice(2), {
-    alias: {
-        t: 'title',
-        p: 'program'
-    }
+const fs = require('fs')
+const walk = require('walk')
+const moment = require('moment')
+const _ = require('underscore')
+const path = require('path')
+const shell = require('shelljs')
+const cli = require('meow')([`
+  Usage
+    $ journall <input>
+
+  Options
+    -t, --title A token
+    -p, --program The program to open with
+    --path The path to the folder (Can be specifed with $JOURNALL)
+
+  Examples
+    $ get-github-user -t 'Test'
+    # Will open a document with test
+`], {
+  alias: {
+    t: 'token',
+    p: 'program'
+  }
 })
 
-var journalFolder = (path.resolve(process.env['JOURNALL']))
-var files  = []
+var journalFolder = cli.flags.path || path.resolve(process.env['JOURNALL'])
+var files = []
 var filename = moment().format('YYYY.MM.DD') + '.md'
-var program = argv.p || process.env['JOURNALL_PROGRAM']
+var program = cli.flags.program || process.env['JOURNALL_PROGRAM']
+const title = cli.flags.title || cli.input
 
 function openFile (text) {
   console.log(text + ' Opening page.')
@@ -25,37 +37,36 @@ function openFile (text) {
     {silent: false}).output
 }
 
-function addCustomHeader (header) {
-  header = '\n\n## ' + header + '\n'
+function wrapTitle (title) {
+  return `\n\n## ${title}\n`
+}
 
-  return fs.appendFile(journalFolder + '/' + filename, header, function(err) {
-    if (err) console.log('Writing the new header did not work', err)
+function addCustomHeader (title) {
+  return fs.appendFile(path.resolve(journalFolder + '/' + filename), wrapTitle(title), function (err) {
+    if (err) {
+      console.log('Writing the new header did not work', err)
+    }
     openFile('Date already exists.')
   })
 }
 
 // Read all files
-var walker  = walk.walk(journalFolder, { followLinks: false })
-walker.on('file', function(root, stat, next) {
-    files.push(stat.name)
-    next()
+var walker = walk.walk(journalFolder, { followLinks: false })
+walker.on('file', function (root, stat, next) {
+  files.push(stat.name)
+  next()
 })
 
-walker.on('end', function() {
-
-  var header
-
+walker.on('end', function () {
+  var header = `# ${moment().format('YYYY MMMM Do')}\n`
   if (!_.contains(files, filename)) {
-
     // Add the date and an optional title to the header
-    header = '# ' + moment().format('YYYY MMMM Do') + '\n'
-    if (argv.t && typeof argv.t === 'string' ||
-      argv._[0] && typeof argv._[0] === 'string') {
-      header += '\n## ' + (argv.t || argv._[0]) + '\n'
+    if (title) {
+      header += wrapTitle(title)
     }
 
     // Create file with simple header
-    fs.writeFile(journalFolder + '/' + filename, header, function (err) {
+    fs.writeFile(path.resolve(journalFolder + '/' + filename), header, function (err) {
       if (err) {
         return new Error(err)
       }
@@ -63,16 +74,7 @@ walker.on('end', function() {
       openFile('Log entered.')
     })
   } else {
-
     // Add the optional title at the end of the file
-    if (argv.t && typeof argv.t === 'string') {
-      addCustomHeader(argv.t)
-    } else if (argv._ && _(argv._).every( function(arg) {
-      return _.isString(arg)
-    })) {
-      addCustomHeader(argv._.join(' '))
-    } else {
-      openFile('Date already exists.')
-    }
+    (title) ? addCustomHeader(title) : openFile('Date already exists.')
   }
 })
